c737d6d26d0e053a1c350c1a2a0ecbdcd6aa7138
gerardb83 <gbreitenbeck@gmail.com>
2020-02-09 17:04:21 -0500

    refactored tests to include setup

--- src/test/java/co/grandcircus/stringCalculator/AppTest.java
+++ src/test/java/co/grandcircus/stringCalculator/AppTest.java
@@ -2,20 +2,25 @@ package co.grandcircus.stringCalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
+	private Calculator calculator;
+	
+	@BeforeEach
+	public void setup() {
+		calculator = new Calculator();
+	}
 	
 	@Test
 	public void whenEmptyIsPassedReturns0() {
-		Calculator calculator = new Calculator();
 		assertEquals(0, calculator.add(""));
 	}
 
 	@Test
 	public void whenOnePositiveNumberIsPassedReturnsThatNumber() {
-		Calculator calculator = new Calculator();
 		assertEquals(1, calculator.add("1"));
 		assertEquals(10, calculator.add("10"));
 	}
